a692760e608ae012f5693fa950045cd10694177e
avon <avon@gmx.de>
2024-12-09 22:19:27 +0100

    do some funny stuff

--- ressource/shader/vertex.glsl
+++ ressource/shader/vertex.glsl
@@ -2,9 +2,17 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
+
+uniform float time;
+
 out vec3 vertexColor;
 
 void main(){
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    vertexColor = aColor;
+
+   // Bewegung entlang der x-Achse
+    float translation = mod(time * 0.5, 2.0) - 1.0; // Bewegung von -1 bis +1
+    float wave = sin((aPos.x + translation) * 3.14159); // Sinus-Wellenform
+
+    // Position des Vertex setzen
+    gl_Position = vec4(aPos.x + translation, aPos.y + wave * 0.1, aPos.z, 1.0); // Amplitude: 0.1
 }
